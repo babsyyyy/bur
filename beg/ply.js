f042830ac6fff0fab4b46c8378875d1e5a745e2a
babsyyyy <babalolaisrael24@gmail.com>
2022-10-05 13:01:35 +0100

    second commit

--- beg/ply.js
+++
@@ -1,8 +0,0 @@
-let one = document.querySelector(".one");
-let menu = document.querySelector(".menu")
-one.addEventListener ("click", function(){
-    if(menu.style.display == "none"){
-        menu.style.display = "block"
-    }else{menu.style.display = "none"}
-})
-console.log("i am a boy")
